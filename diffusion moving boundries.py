added fixed gradient boundry condititon

diffusion moving boundries.py
@@ -18,18 +18,27 @@ import matplotlib.pyplot as plt
 def diffusion(x, h, D): #diffusion in x direction
     uiPlus1 = np.copy(x)
     uiMinus1 = np.copy(x)
-    for y in range(len(x[:,0])):
-        uiPlus1[y,:] = np.append(x[y,1:],x[y,len(x)-1])
-        uiMinus1[y,:] = np.append(100,x[y,:-1])
+    for i in range(len(x[:,0])):
+        uiPlus1[i,:] = np.append(x[i,1:],x[i,len(x[0,:])-1])
+        uiMinus1[i,:] = np.append(100,x[i,:-1])
     return (np.multiply(D,(uiPlus1 + -2*x + uiMinus1) / h**2))
 
-def periodicdiffusion(x,h,D): #diffusion in y direction
+def periodicdiffusion(x, h, D): #diffusion in y direction
     uiPlus1 = np.copy(x)
     uiMinus1 = np.copy(x)
-    for y in range(len(x[0,:])):
-        uiPlus1[:,y] = np.append(x[1:,y],x[:1,y])
-        uiMinus1[:,y] = np.append(x[-1:,y],x[:-1,y])
+    for i in range(len(x[0,:])):
+        uiPlus1[:,i] = np.append(x[1:,i],x[:1,i])
+        uiMinus1[:,i] = np.append(x[-1:,i],x[:-1,i])
     return (np.multiply(D,(uiPlus1 + -2*x + uiMinus1) / h**2))
+
+def mirrordiffusion(x, h, D): #for y direction half boundry
+    uiPlus1 = np.copy(x)
+    uiMinus1 = np.copy(x)
+    for i in range(len(x[0,:])):
+        uiPlus1[:,i] = np.append(x[1:,i],x[len(x[:,0])-1,i])
+        uiMinus1[:,i] = np.append(x[0,i],x[:-1,i])
+    return (np.multiply(D,(uiPlus1 + -2*x + uiMinus1) / h**2))
+
 
 
 #%% RUNGE KUTTA METHODS
@@ -41,35 +50,55 @@ def RK4(x, dt, dx, D):
     fd = diffusion(x + fc*dt, dx, D)
     return x + 1/6 *(fa + 2*fb + 2*fc + fd) * dt
 
-def periodicRK4(x,dt,dx,D):
+def periodicRK4(x, dt, dx, D):
     fa = periodicdiffusion(x, dx, D)
     fb = periodicdiffusion(x + fa*dt/2, dx, D)
     fc = periodicdiffusion(x + fb*dt/2, dx, D)
     fd = periodicdiffusion(x + fc*dt, dx, D)
     return x + 1/6 *(fa + 2*fb + 2*fc + fd) * dt
 
+def mirrorRK4(x, dt, dx, D):
+    fa = mirrordiffusion(x, dx, D)
+    fb = mirrordiffusion(x + fa*dt/2, dx, D)
+    fc = mirrordiffusion(x + fb*dt/2, dx, D)
+    fd = mirrordiffusion(x + fc*dt, dx, D)
+    return x + 1/6 *(fa + 2*fb + 2*fc + fd) * dt
+
+#EULER METHODS
+
+def euler(yn, stepsize, t, h):
+    return(yn + (stepsize * diffusion(t, yn, h)))
+
+def mirroreuler(x, dt, dx, D):
+    return(x + (dx * mirrordiffusion(x, dx,)))
+
+def periodiceuler(yn, stepsize, t, h):
+    return(yn + (stepsize * periodicdiffusion(t, yn, h)))
+
 
 #%% Initializing
 
 def initial_left(x): #unsaturated oxygen
-    s = (int((xmax-xmin)/h),int((xmax-xmin)/h))
-    y = np.zeros(s)
-    y[:,5] = 100
+    stemp = (int((ymax-ymin)/h),int((xmax-xmin)/h))
+    y = np.zeros(stemp)
     return(y)
 
 #Setting step size
 
-h=0.1
-xmin=0
-xmax=1
-dt=0.001
-s=(int((xmax-xmin)/h),int((xmax-xmin)/h))
+h=0.1 #SPATIAL STEP SIZE
+xmin=0 
+xmax=1 #INITIAL RANGE BETWEEN OXYGEN AND ALLUMINIUM
+ymin=0
+ymax=0.5 #DISTANCE IN BULK DIRECTION
+dt=0.001 #TEMPORAL STEP SIZE
+
+s=(int((ymax-ymin)/h),int((xmax-xmin)/h)) #number of nodes (used in defining D matrix)
 x = np.linspace(int(xmin),int(xmax),int((xmax-xmin)/h)) #this just defines the axis for plots
 y = np.linspace(int(xmin),int(xmax),int((xmax-xmin)/h)) #this is only used in 3d plotting
 
 # MATRIX OF DIFFUSION COEFICIENTS
 D = np.ones(s)
-for i in range (1,int((xmax-xmin)/h)):
+for i in range (1,int((ymax-ymin)/h)):
     D[i,:] = 0.01
 
 #Setting the starting conditions
@@ -79,9 +108,8 @@ u = initial_left(x)
 
 print('x=',x)
 plt.figure(1)
-plt.plot(x,u)
-print('x=', x)
-print('y=', u)
+plt.plot(x,u[0,:])
+print('y=', u[0,:])
 plt.show()
 
 
@@ -112,11 +140,10 @@ plt.show()
     
 
 #%%This loop is for 3D
-
 for i in range (0,100000):
-    u = RK4(u, dt, h, D)
-    u = periodicRK4(u, dt, h, D)
     u[:,0] = 100
+    u = RK4(u, dt, h, D)
+    u = mirrorRK4(u, dt, h, D)
     
     # if i%50 == 0: THIS IS FOR A 3D PLOT
     #     fig = plt.figure()
@@ -138,6 +165,6 @@ for i in range (0,100000):
         u = np.c_[u,np.zeros(len(u[:,0]))]
         xmax = xmax + h
         x = np.append(x,xmax+h)
-        D = np.c_[D,[1,0.01,0.01,0.01,0.01,0.01,0.01,0.01,0.01,0.01]]
+        D = np.c_[D,[1,0.01,0.01,0.01,0.01]]
 
         